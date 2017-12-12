8acae9bf5e4258765415c15376fea8513be354c3
JAIME <lldjghettoll@gmail.com>
2017-12-11 19:01:11 -0500

    Proyecto elaborado por Jaime Wong

---
+++ TUTORIAL/src/Paquete/TutorialGithub.java
@@ -0,0 +1,10 @@
+package Paquete;
+
+public class TutorialGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Buenos dias con todos!!!");
+	}
+
+}
