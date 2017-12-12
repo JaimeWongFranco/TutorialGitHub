3bf8139c2f917f2b357032173d2a17d60abcf5e0
MaleniRayme <34254717+MaleniRayme@users.noreply.github.com>
2017-12-12 01:18:47 -0500

    MaleniRayme
    Trabajo editado

--- TUTORIAL/src/Paquete/TutorialGithub.java
+++ TUTORIAL/src/Paquete/TutorialGithub.java
@@ -5,6 +5,7 @@ public class TutorialGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos dias con todos!!!");
+		System.out.println("Trabajo colaborativo");
 	}
 
 }
